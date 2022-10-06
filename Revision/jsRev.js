c01c3dfd396eff712cd72682fac07182a854b3fe
bicky <bickybong@gmail.com>
2022-10-06 10:52:53 +0800

    revisions on py and js

---
+++ Revision/jsRev.js
@@ -0,0 +1,43 @@
+// if
+
+// let x=6;
+// if (x<5){
+//     console.log("smoll");
+// } else{
+//     console.log("huge");
+// }
+
+// function logger(num){
+//     console.log(`I love num ${num}`);
+// }
+// logger(6)
+
+// string
+// let str = "Deeeeez nuts"
+// newStrStart = str.slice(4)
+// newStrEnd = str.slice(0,-3)
+//  console.log(newStrStart);
+
+// array
+// const arr = [0,1,2,3,4,"fsd","hds"]
+// arr.push("gsd")
+// let last = arr.pop()
+// let first = arr.shift()
+// arr.unshift("Im back")
+// let mid = arr.slice(1,4)
+// for (let ele of arr){
+//     console.log(ele);
+// }
+
+
+// objects
+// const arr = ["first",0,1,2,3,4,"fsd","hds"]
+// let obj = {}
+// for (let i=0; i<arr.length; i++){
+//     obj[arr[i]] = i
+// }
+
+// for (let key in obj){
+//     console.log(key);
+// }
+
---
+++ Revision/pyRev.py
@@ -0,0 +1,42 @@
+
+# if
+# x = 6
+# if x < 5:
+#     print("smolll")
+# else:
+#     print("huge")
+
+# function
+# def logger(num):
+#     print("I love num " + str(num))
+# logger(6)
+
+# Array
+# arr = [0,1,2,3,4,5,6,"jdfg"]
+# arr.append("last")
+# last = arr.pop()
+
+# for i in range(0,len(arr)):
+#     print(arr[i])
+
+# for num in arr:
+#     print(num)
+
+# for i,e in list(enumerate(arr)):
+#     print(i,e)
+
+
+# String
+# str = "I love food"
+
+# for letter in str:
+#     print(letter)
+
+# Dictionary
+# arr = [0,1,2,3,4,5,6,"jdfg",1]
+# dic = {}
+# for num in arr:
+#     dic[num] = dic.get(num,0) + 1
+
+# for key, value in dic.items():
+#     print(key,value)
