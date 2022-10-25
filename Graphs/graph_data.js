1d921ec82e8e54c50addf5283d5008610557a019
bicky <bickybong@gmail.com>
2022-10-25 10:51:12 +0800

    graphs done

---
+++ Graphs/graph_data.js
@@ -0,0 +1,13 @@
+// edge list
+const graph = [[0, 2], [2, 3], [2, 1], [1,3]];
+
+// adjacent list 
+const graph = [[2], [2,3], [0,1,3], [1,2]]
+
+// adjacent matrix
+const graph = {
+    0: [0, 0, 2, 0],
+    1: [0, 0, 1, 1],
+    2: [1, 1, 0, 1],
+    3: [0, 1, 1, 0]
+}
