c3eb5ac8c404b81014c0d514f05d9a3aa60951e6
bicky <bickybong@gmail.com>
2022-10-28 12:00:03 +0800

    search dones

---
+++ Search/LinearSearch.js
@@ -0,0 +1,5 @@
+var num = [1,2,3,4,5,6,7];
+
+console.log(num.indexOf(7));
+
+// Linear search O(n) worst case
--- Sort/sort().js
+++ Sort/sort().js
@@ -1,5 +1,6 @@
 const nums = [5, 7, 1, 22, 78, 43, 75];
 
+// normal sort no work as only looks at first charcode
 console.log(nums.sort(function (a, b){
     return a - b
 }));
